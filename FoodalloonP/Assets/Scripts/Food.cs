b22f3deeb726f3c43dbf0a69187d3d7523e50331
Ariel <ar.i117@hotmail.com>
2018-03-01 16:49:42 -0600

    Cambio en posicion de la comida

--- FoodalloonP/Assets/Scripts/Food.cs
+++ FoodalloonP/Assets/Scripts/Food.cs
@@ -5,11 +5,11 @@ using UnityEngine;
 public class Food : MonoBehaviour {
 
     Rigidbody2D rb;
-    public float startForce = 0.1f;
+    public float startForce;
 
 	void Start () {
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(transform.up * -startForce, ForceMode2D.Impulse);
+        rb.AddForce(transform.up * startForce, ForceMode2D.Impulse);
 	}
 
 	void Update () {
