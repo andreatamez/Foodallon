bd99f3319882bf55dc446bb4b3ec9b7d64f44b1e
Ariel <ar.i117@hotmail.com>
2018-03-05 17:43:51 -0600

    Timer agregado

---
+++ FoodalloonP/Assets/Scripts/Timer.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour {
+    public float timeLeft = 5.0f;
+    public Text text;
+	
+	// Update is called once per frame
+	void Update () {
+        timeLeft -= Time.deltaTime;
+        text.text = "Time: " + Mathf.Round(timeLeft) + "s";
+        if (timeLeft < 0)
+        {
+            //Application.loadedLevel();
+            Debug.Log("END");
+        }
+	}
+}
