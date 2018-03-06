d2dd8c8cdd8b6f2d94420e86837166c0d3436dbb
Ariel <ar.i117@hotmail.com>
2018-03-06 12:05:09 -0600

    comida se elimina al choque

--- FoodalloonP/Assets/Scripts/Player.cs
+++ FoodalloonP/Assets/Scripts/Player.cs
@@ -17,4 +17,17 @@ public class Player : MonoBehaviour {
             rb.velocity = Vector2.up * jumpForce;
         }
 	}
+
+    public void OnTriggerEnter2D(Collider2D col)
+    {
+
+        if (col.tag == "Food")
+        {
+            Debug.Log("crash");
+            //rb.gravityScale += 0.5f;
+            Destroy(col.gameObject);
+            return;
+        }
+
+    }
 }
