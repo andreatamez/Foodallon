c2dec298d386aabe6e9a28125bc6edf125dbe2d7
Ariel <ar.i117@hotmail.com>
2018-03-14 19:02:33 -0600

    periodo para tirar maletas creado

--- FoodalloonP/Assets/Scripts/CaseSpawner.cs
+++ FoodalloonP/Assets/Scripts/CaseSpawner.cs
@@ -5,19 +5,32 @@ using UnityEngine;
 public class CaseSpawner : MonoBehaviour {
 
     public GameObject casePrefab;
-
+    public float timePeriod = 10f;
     public Transform[] spawnPoints;
     public float min = .1f;
     public float max = 1f;
 
 
     void Start()
+    {    
+        StartCoroutine(period());
+    }
+
+    IEnumerator period()
     {
-        StartCoroutine(SpawnCase());
+        while (true)
+        {
+            StartCoroutine("SpawnCase");
+            yield return new WaitForSeconds(10f);
+            Debug.Log("stop");
+            StopCoroutine("SpawnCase");
+            yield return new WaitForSeconds(10f);
+        } 
     }
 
     IEnumerator SpawnCase()
     {
+        Debug.Log("start");
         while (true)
         {
             float delay = Random.Range(min, max);
@@ -25,10 +38,7 @@ public class CaseSpawner : MonoBehaviour {
             int index = Random.Range(0, spawnPoints.Length);
             Transform point = spawnPoints[index];
             GameObject caseObject = Instantiate(casePrefab, point.position, point.rotation);
-            Destroy(caseObject, 5f);
+            Destroy(caseObject, 4f);
         }
     }
-
-
-
 }
