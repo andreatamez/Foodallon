589d8bd7765f96926138e5936fc74725889ae2ea
Ariel Méndez <arielmendez735@gmail.com>
2018-04-17 11:17:43 -0500

    cambio en los archivos

---
+++ FoodalloonP/Assets/Scripts/StartScreen.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScreen : MonoBehaviour {
+
+    public AudioClip startClip;
+    private AudioSource audioStart;
+
+    void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            StartCoroutine(Press());
+        }
+    }
+
+    public AudioSource LoadClips(AudioClip clip)
+    {
+        AudioSource newAudio = gameObject.AddComponent<AudioSource>();
+        newAudio.clip = clip;
+        return newAudio;
+    }
+
+    public void Awake()
+    {
+        audioStart = LoadClips(startClip);
+    }
+
+    IEnumerator Press()
+    {
+        audioStart.Play();
+        yield return new WaitForSeconds(1.5f);
+        ChangeScene("Menu");
+    }
+
+    public void ChangeScene(string scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
