aa0cefb2a7e8a5ef33160bb88213d6a5596fc21a
Ariel Méndez <arielmendez735@gmail.com>
2018-05-10 23:29:57 -0500

    prueba

--- FoodalloonP/Assets/Scripts/GameVars.cs
+++ FoodalloonP/Assets/Scripts/GameVars.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GameVars : MonoBehaviour {
+
     public static int points = 0;
     public static int rottenPoints = 0;
     public static float foodForce = 20;
